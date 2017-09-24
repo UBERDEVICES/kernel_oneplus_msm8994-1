e9682c91ff7656de38c634f4e1971b1cf1e3e56f
RJ Trujillo <CertifiedBlyndGuy@gmail.com>
2017-09-24 19:36:38 +0200

    build: Mark-IX: The number 9 does exist.
    1, 2, 3, 4, 5, 6, 7, 8, 10... Come on Apple and MicroSoft
    
    * Fixed scheduler jank
    * Caught up with Kawori

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@
 # Parameters to be configured manually
 #######################################
 
-KAWORI_VERSION="OP2-Mark-VIII"
+KAWORI_VERSION="OP2-Mark-IX"
 
 TOOLCHAIN="../g49/bin/aarch64-linux-android-"
 ARCHITECTURE=arm64
